b16302d3c7a15e92dacfafa339d298d05ea79be2
mezhi <316819375@qq.com>
2020-03-06 19:30:33 +0800

    第三次提交

--- src/components/ShowBlogs.vue
+++ src/components/ShowBlogs.vue
@@ -3,7 +3,9 @@
         <h1>博客总览</h1>
         <input type="text" v-model="search" placeholder="请输入搜索内容">
         <div v-for="blog in filteredBlogs" class="single-blog">
+          <router-link v-bind:to="'/blog/'+blog.id">
            <h2 v-rainbow>{{blog.title | toUppercase}}</h2>
+          </router-link>
            <article>{{blog.body | snippet}}</article>
         </div>
     </div>
@@ -34,6 +36,7 @@ export default {
         })
       }
     },
+
     filters:{
       // "to-uppercase":function(){
       //   return value.toUpperCase();
@@ -63,5 +66,15 @@ export default {
     margin: 20px 0;
     box-sizing: border-box;
     background: #eee;
+    border: 1px dotted #aaa;
+}
+#show-blogs a{
+    color: #444;
+    text-decoration: none;
+}
+input[type="text"]{
+    padding: 8px;
+    width: 100%;
+    box-sizing: border-box;
 }
 </style>
